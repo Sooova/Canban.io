acaf579e29eec96c7c7b756f19bd6536b77a4b72
Sooova <83949034+Sooova@users.noreply.github.com>
2021-11-24 17:12:39 +1100

    Added profile.js
    Added in profile page component structure
    !canban.new(Add Profile Page, toDo)

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,13 @@
+//add in profile page
+import React, { useState } from 'react';
+import Auth from '../utils/auth';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Profile = function() {
+    return (
+        <>
+
+        </>
+    )
+}
